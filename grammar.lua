1b0b7cf5ca6c313ae1ae64b98a50193316bde58e
Tanner Grehawick <tgrehawi@gmail.com>
2017-04-05 04:58:16 -0400

    parser recognizes indented blocks and line-end comments

---
+++ grammar.lua
@@ -0,0 +1,43 @@
+re = require "re"
+
+g = re.compile([[
+   block <- {|
+      {:ident: ichar*:} line
+      (
+         blankline / 
+         (=ident !ichar line) /
+         &(=ident ichar) block
+      )*
+   |}
+   
+   line <- {[^%nl/]*} (%nl / comment)
+   ichar <- (%tab / " ")
+   blankline <- ichar* (%nl / comment)
+   comment <- ("//" [^%nl]* %nl )
+  
+]], {tab = "\t"})
+
+local function examine(t, indent)
+   indent = indent or ""
+   print (indent.."{")
+   local oldindent = indent
+   indent = indent.."  "
+   for _, x in ipairs(t) do
+      if type(x) == "string" then
+         print(indent..'"'..x..'"')
+      elseif type(x) == "table" then
+         examine(x, indent)
+      end
+   end
+   print(oldindent.."}")
+end
+
+local file, error = io.open("sample/vect.src")
+if file then
+   src = file:read("*all")
+   print(src) 
+   ast = g:match(src)
+   examine(ast)
+else
+   error(error)
+end
